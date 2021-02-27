a72472313a94a5c67b909c0040c29f4cb7dbf1e9
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-02-27 15:32:30 -0500

    data filter complete
    moving to level 2

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,27 +1,31 @@
 // import data from data.js
 var tableData = data;
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 // Check
 //console.log(tableData);
+
+// Add names for columns
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
 // Reference the html table-(table body)
 var tbody = d3.select("tbody");
 
 // Connect values to columns 
-var initialLoadData = (startdata) => {
-    startdata.forEach(function (ufoSighting) {
+var loadData = (startData) => {
+    startData.forEach(function (ufoSighting) {
         //console.log(ufoSighting);
         var row = tbody.append("tr");
-        columns.forEach(column => row.append("td").text(ufoSighting[column]))
+        columns.forEach(function(column) {
+            row.append("td").text(ufoSighting[column])
+        });
     });
 }
 // Show whole table at start (before filtering)
-initialLoadData(tableData);
+loadData(tableData);
 
 // Create button
 var button = d3.select("#filter-btn");
 // Tell event listener what to do 
-button.on("click", function() {
+button.on("click", function () {
     // Empty table if already populated
     tbody.html("");
     //  Select the input date get the raw HTML nodes
@@ -29,9 +33,14 @@ button.on("click", function() {
     // Get the value property of the input date
     var inputValue = inputElement.property("value");
     //console.log(inputValue);
-    
     // Filter Data with datetime equal to input value
-    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
-    //console.log(filteredData);
-    filteredData.
+    var filteredData = tableData.filter(recordedEvent => recordedEvent.datetime === inputValue);
+    // Show in console date tried and number of sightings
+    console.log(`The total number of sightings on ${inputValue} is: ${filteredData.length}`);
+    // If functuon to help error processsing and give info to user
+    if (filteredData.length != 0) {
+        loadData(filteredData);
+    } else {
+        tbody.append("tr").append("td").text("No data found for this date.... Please try to search between 1/1/2010 -> 1/13/2010");
+    }
 });
