8ce749d11a348c95d856212407d7cf23c669020e
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-02-26 20:11:40 -0500

    test commit
    add images and start app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,14 @@
 // import data from data.js
 var tableData = data;
 
+// Check
+//console.log(tableData);
 
+// Reference the html table-(table body)
+var tbody = d3.select("tbody");
+
+// Connect values to columns 
+
+// Create event listener / select button
+
+// filter-- date time
