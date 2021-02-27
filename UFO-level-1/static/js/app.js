72adf4bde853ecf2fa3edd4c57aa7077fd42d5fd
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-02-26 22:01:02 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,6 +1,6 @@
 // import data from data.js
 var tableData = data;
-
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 // Check
 //console.log(tableData);
 
@@ -8,7 +8,24 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Connect values to columns 
+var loadData = (newdata) => {
+    newdata.forEach(function (ufoSighting) {
+        console.log(ufoSighting);
+        var row = tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSighting[column])
+        )
+    });
+}
+
+loadData(tableData);
 
 // Create event listener / select button
+var button = d3.select("#filter-btn");
+button.on("click", function() {
 
+    // Empty table if already populated
+    tbody.html("");
+
+    // 
+});
 // filter-- date time
