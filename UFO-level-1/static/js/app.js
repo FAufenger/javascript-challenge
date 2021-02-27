eda64134358a447231a3d4d523f523fd8b0d4db4
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-02-27 09:41:58 -0500

    Update app.js

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -21,11 +21,19 @@ loadData(tableData);
 
 // Create event listener / select button
 var button = d3.select("#filter-btn");
+
 button.on("click", function() {
 
     // Empty table if already populated
     tbody.html("");
-
-    // 
+    //  Select the input date get the raw HTML nodes
+    var inputElement = d3.select("#datetime");
+    // Get the value property of the input date, state, shape
+    var inputValue = inputElement.property("value");
+    // console.log input value
+    console.log(inputValue);
+    // Filter Data with datetime equal to input value
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+    // console.log filter values
+    console.log(filteredData);
 });
-// filter-- date time
