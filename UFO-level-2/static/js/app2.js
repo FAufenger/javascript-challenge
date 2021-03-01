c97cc0937cfbfed6d98def99dfcecde6629e9b2e
Fox IV <73198491+FAufenger@users.noreply.github.com>
2021-03-01 10:48:20 -0500

    add log for app2
    add || to filter

--- UFO-level-2/static/js/app2.js
+++ UFO-level-2/static/js/app2.js
@@ -23,24 +23,69 @@ var loadData = (startData) => {
 loadData(tableData);
 
 // Create button
-var button = d3.select("#filter-btn");
+var buttonFilter = d3.select("#filter-btn");
 // Tell event listener what to do 
-button.on("click", function () {
+buttonFilter.on("click", function () {
     // Empty table if already populated
     tbody.html("");
-    //  Select the input date get the raw HTML nodes
-    var inputElement = d3.select("#datetime");
-    // Get the value property of the input date
-    var inputValue = inputElement.property("value");
-    //console.log(inputValue);
-    // Filter Data with datetime equal to input value
-    var filteredData = tableData.filter(recordedEvent => recordedEvent.datetime === inputValue);
-    // Show in console date tried and number of sightings
-    console.log(`The total number of sightings on ${inputValue} is: ${filteredData.length}`);
-    // If functuon to help error processsing and give info to user
-    if (filteredData.length != 0) {
-        loadData(filteredData);
-    } else {
-        tbody.append("tr").append("td").text("No data found for this query.... Please try to search again");
+
+    //  Select the inputs and get the raw HTML nodes
+    // Date
+    var inputDateElement = d3.select("#datetime");
+    var inputDateValue = inputDateElement.property("value");
+    // City
+    var inputCityElement = d3.select("#cityName");
+    var inputCityValue = inputCityElement.property("value");
+    // State
+    var inputStateElement = d3.select("#stateName");
+    var inputStateValue = inputStateElement.property("value");
+    // Country
+    var inputCountryElement = d3.select("#countryName");
+    var inputCountryValue = inputCountryElement.property("value");
+    // Shape
+    var inputShapeElement = d3.select("#shapeType");
+    var inputShapeValue = inputShapeElement.property("value");
+
+    // Consle log check variable inputs functioning
+    var recordSearchLog = (searchCriteria) => {
+        if (searchCriteria != "") {
+            console.log(`Search Criteria: ${searchCriteria}`);}
     }
+    recordSearchLog(inputDateValue);
+    recordSearchLog(inputCityValue);
+    recordSearchLog(inputStateValue);
+    recordSearchLog(inputCountryValue);
+    recordSearchLog(inputShapeValue);
+
+    // Filter Data with datetime equal to input value
+  
+    var filteredData = tableData.filter(recordedEvent => recordedEvent.datetime === inputDateValue ||
+                                                         recordedEvent.city === inputCityValue ||
+                                                         recordedEvent.state === inputStateValue ||
+                                                         recordedEvent.country === inputCountryValue ||
+                                                         recordedEvent.shape === inputShapeValue);
+        
+
+    console.log(filteredData);
+    loadData(filteredData);
+        // Show in console date tried and number of sightings
+        // console.log(`The total number of sightings on ${inputDateValue} is: ${filteredData.length}`);
+        // // If functuon to help error processsing and give info to user
+        // if (filteredData.length != 0) {
+        //     loadData(filteredData);
+        // } else {
+        //     tbody.append("tr").append("td").text("No data found for this query.... Please try to search again");
+        // }
 });
+
+var buttonReset = d3.select("#reset-btn");
+// Tell event listener what to do 
+buttonReset.on("click", function () {
+    // Empty table if already populated
+    tbody.html("");
+    // Reload entire table
+    loadData(tableData);
+    // Show in console number of sightings
+    console.log(`The total number of sightings currently in the database: ${data.length}`);
+});
+
